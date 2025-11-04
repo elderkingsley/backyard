342818f8739962db57ce2a157f9f3f52d3c555fc
elderkingsley <elderkingsley@gmail.com>
2025-11-04 18:57:06 +0100

    6th commit

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 Route::middleware('guest')->controller(AuthController::class)->group(function() {
 Route::get('/register', 'showRegister')->name('show.register');
 Route::get('/login', 'showLogin')->name('show.login');
-Route::post('/register', 'register')->name('register');
+//Route::post('/register', 'register')->name('register');
 Route::post('/login', 'login')->name('login');
 });
 
